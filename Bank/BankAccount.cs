85762e80e8a7446d9b77d9532b1494385373251e
rosenclever.gazoni <rosenclever.gazoni@TPCP04LAB0900>
2025-10-01 20:37:59 -0300

    Adicionar arquivos de projeto.

---
+++ Bank/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class BankAccount
+    {
+        private string _customerName;
+        private double _balance;
+
+        public BankAccount(string customerName, double balance)
+        {
+            _customerName = customerName;
+            _balance = balance;
+        }
+
+        public string CustomerName 
+        {
+            get
+            {
+                return _customerName;
+            }
+            private set
+            {
+                _customerName = value;
+            } 
+        }
+    }
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,7 @@
+﻿using Bank;
+
+BankAccount account1 = new BankAccount("Rosen", 1000000.99);
+
+Console.WriteLine("O nome do cliente da conta 1 é ", account1.CustomerName);
+
+
