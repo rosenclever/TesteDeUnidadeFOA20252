781087198011f18495489d0808b53364c4cecfee
rosenclever <rosenclever@gmail.com>
2025-10-22 20:28:19 -0300

    ajustes no program

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -1,13 +1,13 @@
 ﻿using Bank;
 
-Customer customer1 = new Customer("Rosen", "11111111");
+Customer customer1 = new Customer("Rosen", "11111111", "12345678901", 2000);
 BankAccount account1 = new BankAccount(customer1, 123, 1000000.99);
 
 Console.WriteLine("O nome do cliente da conta 1 é ", account1.Customer.Name);
 
 Console.WriteLine("O saldo da conta 1 é ", account1.Balance);
 
-Customer customer2 = new Customer("Pedro", "2222222");
+Customer customer2 = new Customer("Pedro", "2222222", "10987654321", 2004);
 BankAccount account2 = new BankAccount(customer1, 654321, 2341.42);
 
 double saldoTotalInicial = account2.Balance + account1.Balance;
